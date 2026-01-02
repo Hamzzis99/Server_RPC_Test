7aecaccbd3d54114c1d345a053ea57c8ffac3368
Gihyeon <hamzzi99@naver.com>
2026-01-03 03:56:45 +0900

    원격과 로컬 액터 구분 매크로

--- Source/ArenaBattle/ArenaBattle.h
+++ Source/ArenaBattle/ArenaBattle.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "Engine/Engine.h"
 
+#define LOG_LOCALROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole())) // 로컬 역할 정보를 로그에 표시하기 위한 매크로
+#define LOG_REMOTEROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole())) // 원격 역할 정보를 로그에 표시하기 위한 매크로 (서버)
 // 네트워크 모드 정보를 로그에 표시하기 위한 매크로
 #if WITH_EDITOR
 #define LOG_NETMODEINFO ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("CLIENT%d"), GetWorld() ? GetWorld()->GetOutermost()->GetPIEInstanceID() : 0) : \
@@ -20,6 +22,6 @@
 
 // UELOG를 대체할 매크로? 궁금한데? 로그 카테고리, 로그레벨, 포맷, 가변인자 
 // 출력 예시 : LogABNetwork: AABPlayerController::BeginPlay Begin
-#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s][%s / %s] %s %s"), LOG_NETMODEINFO, LOG_LOCALROLEINFO, LOG_REMOTEROLEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
 
 DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
